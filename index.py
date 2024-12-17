27b55c89e52d87c66de27c8d48d22e8288978023
khushdeep5 <babrah.khushdeep268@gmail.com>
2024-12-16 20:44:53 -0700

    created class 2

--- index.py
+++ index.py
@@ -45,3 +45,59 @@ class Doctor:
     
     def __str__(self):
         return f"{self._doctor_id}_{self._name}_{self._specialization}_{self._working_time}_{self._qualification}_{self._room_number}"
+    
+class DoctorManager():
+    #contrusctor
+    def __init__(self):
+        self.doctors=[]
+        self.read_doctor_file()
+
+    #formatiing doctors info
+    def format_dr_info(doctor):
+       return f"{doctor.doctor_id}_{doctor.name}_{doctor.specialization}_{doctor.working_time}_{doctor.qualification}_{doctor.room_number}"
+
+    #entering doctor info
+    def enter_dr_info(self):
+        doctor_id=int(input("Enter the doctor’s ID: "))
+        name=input("Enter the doctor’s name: ")
+        speciality=input("Enter the doctor’s specility: ")
+        working_time=input("Enter the doctor’s timing (e.g., 7am-10pm): ")
+        qualification=input("Enter the doctor’s qualification: ")
+        room_number=int(input("Enter the doctor’s room number: "))
+
+        doctor=Doctor(doctor_id,name,speciality,working_time,qualification,room_number)
+        return doctor
+
+    #reads doctors.txt
+    def read_doctor_file(self):
+        doctor=open('doctors.txt','r')
+        lines=doctor.readlines()
+
+        for line in lines:
+            entry=line.split('_')
+            doctor_id,name,speciality,working_time,qualification,room_number=entry
+
+            doctor_entry=Doctor(doctor_id,name,speciality,working_time,qualification,room_number)
+
+            self.doctors.append(doctor_entry)
+
+        doctor.close()
+    def search_doctor_by_id(self):
+        doc_id=input('Enter the Doctor ID: ')
+        for doctor in self.doctors:
+            if doctor.get_doctor_id()==doc_id:
+                self.display_doctor_list(doctor)
+                break
+                
+        print(f"Can't find the doctor ...........")
+    def search_doctor_by_name(self):
+        doc_name=input('Enter the Doctor name: ')
+        for doctor in self.doctors:
+            if doctor.get_doctor_name()==doc_name:
+                self.display_doctor_list(doctor)
+                break
+        print(f"Can't find the doctor ...........")
+        
+    def display_doctor_list(self,doctor):
+        print(f'ID    Name                Speciality      Timings          Qualification    Room Number')
+        print(f'{doctor.get_doctor_id():<6}{doctor.get_doctor_name():<20}{doctor.get_specialization():<16}{doctor.get_working_time():<16} {doctor.get_qualification():<16} {doctor.get_room_number()}')
