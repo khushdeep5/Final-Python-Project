0b8f4d9b694087522e15bb9dc3c1309bbd9fe027
khushdeep5 <babrah.khushdeep268@gmail.com>
2024-12-16 21:52:44 -0700

    added class 5 and main function

--- index.py
+++ index.py
@@ -239,7 +239,7 @@ class PatientManager:
         for i in self.patients:
             patient.write(self.format_patient_info(patient) + '\n')
 
-    def add_dr_to_file(self):
+    def add_patient_to_file(self):
         new_pat=self.enter_patient_info()
         self.patients.append(new_pat)
 
@@ -247,6 +247,90 @@ class PatientManager:
         patient.write(self.format_patient_info_for_file(new_pat)+ '\n')
         print(f"\nPatient whose ID is {new_pat.get_p_id()} has been added")
 
+class Management:
+    def __init__(self):
+        self.doctor_manager = DoctorManager()
+        self.patient_manager = PatientManager()
+    
+    def display_menu(self):
+        while True:
+            print("\nWelcome to Alberta Hospital (AH) Managment system")
+            print("Select from the following options, or select 3 to stop:")
+            print("1 - \tDoctors")
+            print("2 - \tPatients")
+            print("3 - \tExit Program")
+            
+            choice = input(">>> ")
+            
+            if choice == '1':
+                self.doctors_menu()
+            elif choice == '2':
+                self.patients_menu()
+            elif choice == '3':
+                print("Thanks for using the program. Bye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+    
+    def doctors_menu(self):
+        while True:
+            print("\nDoctors Menu:")
+            print("1 - Display Doctors list")
+            print("2 - Search for doctor by ID")
+            print("3 - Search for doctor by name")
+            print("4 - Add doctor")
+            print("5 - Edit doctor info")
+            print("6 - Back to the Main Menu")
+            
+            choice = input(">>> ")
+            
+            if choice == '1':
+                self.doctor_manager.display_doctors_list()
+            elif choice == '2':
+                self.doctor_manager.search_doctor_by_id()
+            elif choice == '3':
+                self.doctor_manager.search_doctor_by_name()
+            elif choice == '4':
+                self.doctor_manager.add_dr_to_file()
+            elif choice == '5':
+                self.doctor_manager.edit_doctor_info()
+            elif choice == '6':
+                break
+            else:
+                print("Invalid choice. Please try again.")
+    
+    def patients_menu(self):
+        while True:
+            print("\nPatients Menu:")
+            print("1 - Display patients list")
+            print("2 - Search for patient by ID")
+            print("3 - Add patient")
+            print("4 - Edit patient info")
+            print("5 - Back to the Main Menu")
+            
+            choice = input(">>> ")
+            
+            if choice == '1':
+                self.patient_manager.display_patients_list()
+            elif choice == '2':
+                self.patient_manager.search_patient_by_id()
+            elif choice == '3':
+                self.patient_manager.add_patient_to_file()
+            elif choice == '4':
+                self.patient_manager.edit_patient_info_by_id()
+            elif choice == '5':
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+
+def main():
+    management_system = Management()
+    management_system.display_menu()
+
+
+if __name__ == "__main__":
+    main()
 
 
 
